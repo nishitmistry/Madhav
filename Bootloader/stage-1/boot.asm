dc5b1090a4d340c346306f6f66e28c40fd3608dc
Nishit Mistry <Nishitmistry94@gmail.com>
2026-01-11 00:18:42 +0530

    added boot loader intro which overflows 512 bytes

--- Bootloader/stage-1/boot.asm
+++ Bootloader/stage-1/boot.asm
@@ -1,15 +1,19 @@
 [org 0x7c00] ; this is where the boot loader is started
 [bits 16] ; tell the nasm that we are in 16 bits real mode 
+STACK equ 0x9000
 
+start:
+    xor ax, ax       ; Set all segments to zero
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, STACK    ; Set stack to guarantee data safety
+    call print_text_art
 
-mov bp, 0x9000
-mov sp, bp
-push string
-call print
 jmp $
 %include "./utils/print.asm"
-string:
-    db "Hello, World!", 0
+%include "./Bootloader/strings.asm"
+%include "./Bootloader/stage-1/intro.asm"
 times 510-($-$$) db 0
 dw 0xaa55
 
---
+++ Bootloader/strings.asm
@@ -0,0 +1,10 @@
+madhav_art_1  db "  __  __             _  _                      ", 0
+madhav_art_2 db " |  \/  |           | || |                     ", 0
+madhav_art_3 db " | \  / |  __ _   __| || |__    __ _ __   __   ", 0
+madhav_art_4 db " | |\/| | / _` | / _` || '_ \  / _` |\ \ / /   ", 0
+madhav_art_5 db " | |  | || (_| || (_| || | | || (_| | \ V /    ", 0
+madhav_art_6 db " |_|  |_| \__,_| \__,_||_| |_| \__,_|  \_/     ", 0
+
+; bootloader_stage_1 db "Bootloader stage 1 \r\n ", 0
+created_by db " Created By - Nishit mistry", 0
+contact_creator db " Contact Creator - nishitmistry94@gmail.com", 0
